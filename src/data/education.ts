d954fc563751aba9a0974d1613be28160d7db519
pochih-code <pochihwu1118@gmail.com>
2025-01-15 20:37:02 +0800

    update

--- src/data/education.ts
+++ src/data/education.ts
@@ -11,12 +11,12 @@ export const educationData: Education[] = [
   // If you don't want to show education, just make the array empty.
   {
     year: "September.2024 - Present",
-    institution: "National Yang Ming Chiao Tung University | Hsinchu, Taiwan",
-    degree: "Bachelor of Science in Information Management and Finance | GPA: 4.12/4.3 (Overall/Scale)",
+    institution: "National Yang Ming Chiao Tung University | Taiwan",
+    degree: "Bachelor of Science in Information Management and Finance",
   },
   {
     year: "September.2021 - July.2024",
-    institution: "Taipei Municipal Zhong-Lun High School | Taipei, Taiwan",
-    degree: "GPA: 81.10/100 (Overall/Scale)",
+    institution: "Taipei Municipal Zhong-Lun High School | Taiwan",
+    degree: "General Subjects",
   },
 ];
