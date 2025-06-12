c7127e21734c07afb7d51d94c9aa6354749b74c0
pochih-code <pochihwu1118@gmail.com>
2025-06-12 21:50:45 +0800

    update

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -13,9 +13,10 @@ export interface Publication {
 
 export const publicationData: Publication[] = [
   // If you don't want to show publications, just make the array empty.
+  /*
   {
     year: "2025",
-    conference: " ",
+    conference: "",
     title: "Evaluating the Performance of Open-Vocabulary Object Detection in Low-quality Image",
     authors: "Po-Chih Wu, Chia-Chi Tsai",
     paperUrl: " ",
@@ -24,8 +25,9 @@ export const publicationData: Publication[] = [
     tldr: " In this study, we aim to evaluate the performance of existing models on open-vocabulary object detection tasks under low-quality image conditions.",
     imageUrl:
       "https://storage.googleapis.com/kaggle-datasets-images/7424982/11820620/ab92f283a1415c330b46a0458dc66d0e/dataset-cover.png?t=2025-05-17-13-30-52",
-    award: " ",
+    award: "",
     // if you have an image in public/images, you can use it like this:
     // imageUrl: "/images/publication-image.jpg"
   },
+  */
 ];
